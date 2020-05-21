462cdaef66838dcd381044ec4ad9f01b79e794bf
4Ryanw <a120935779@vip.qq.com>
2020-05-21 23:06:00 +0800

    下载模板

--- src/main/java/cn/cqu/controller/DeviceController.java
+++ src/main/java/cn/cqu/controller/DeviceController.java
@@ -9,8 +9,13 @@ import org.springframework.ui.ModelMap;
 import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.*;
 
+import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
@@ -194,6 +199,38 @@ public class DeviceController {
         String pageName = request.getParameter("pageName");
         String[] headers = request.getParameter("headers").split(",");
         deviceService.exportExcel(response,pageName,headers);
+    }
+
+
+
+    @GetMapping("/template")
+    public void DownloadServlet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+
+        response.setCharacterEncoding("UTF-8");
+        //设置ContentType字段值
+        response.setContentType("text/html;charset=utf-8");
+        //获取所要下载的文件名称
+        String filename = request.getParameter("filename");
+        //下载文件所在目录
+
+        String folder = request.getServletContext().getRealPath("/WEB-INF/statics/file/");
+        //通知浏览器以下载的方式打开
+        response.addHeader("Content-type", "appllication/octet-stream");
+        response.addHeader("Content-Disposition", "attachment;filename="+filename);
+        //通知文件流读取文件
+//        InputStream in = request.getServletContext().getResourceAsStream(folder+filename);
+        InputStream in = new FileInputStream(folder+filename);
+        //获取response对象的输出流
+        OutputStream out = response.getOutputStream();
+        byte[] buffer = new byte[1024];
+        int len;
+        //循环取出流中的数据
+        while((len = in.read(buffer)) != -1){
+            out.write(buffer,0,len);
+        }
+
+
     }
 
 }
